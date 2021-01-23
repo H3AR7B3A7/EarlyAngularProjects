7abad9ec670d879e55bd6ce2d0b4ca2443cad370
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-01-23 16:07:43 +0100

    :rocket: Testprojects

---
+++ testProject/src/app/hero/hero.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent implements OnInit {
+  title = 'Hero';
+  isLiked = true;
+
+  buttonText = 'Click Me!';
+  buttonStyle: string | undefined;
+
+  currentClasses = {
+    star: true,
+    active: false
+  }
+
+  currentStyle1 = 'color: greenyellow; width: 100px'
+
+  currentStyle2 = {
+    color: 'greenyellow',
+    width: '100px'
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public onClick(){
+    this.buttonText = 'That tickles!';
+    this.buttonStyle = 'background-color: lightblue';
+  }
+
+}
