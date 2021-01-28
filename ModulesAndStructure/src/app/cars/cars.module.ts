f850f1948675ad2a82bb9edf93b865f1dfc7712b
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-01-28 20:14:46 +0100

    ✨ Modules example project

---
+++ ModulesAndStructure/src/app/cars/cars.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CarListComponent } from './car-list/car-list.component';
+
+
+
+@NgModule({
+  declarations: [CarListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CarsModule { }
