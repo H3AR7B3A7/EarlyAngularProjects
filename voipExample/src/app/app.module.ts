4da462c864b53ddfe951860efcd15ae0d536f5ea
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-06-10 08:59:32 +0200

    ✨ Header & chat components

--- voipExample/src/app/app.module.ts
+++ voipExample/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material.module';
+import { HeaderComponent } from './header/header.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
