f6be892e119725a3c79153faebe93d88c7b3f84f
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-06-10 09:54:35 +0200

    ✨ Local video stream

--- voipExample/src/app/chat/chat.component.ts
+++ voipExample/src/app/chat/chat.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+const mediaConstraints = {
+  audio: true,
+  video: {width: 720, height: 540}
+}
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements AfterViewInit {
+
+  private localStream!: MediaStream;
+
+  @ViewChild('local_video')
+  localVideo!: ElementRef;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.requestMediaDevices();
   }
 
+  private async requestMediaDevices() {
+    this.localStream = await navigator.mediaDevices.getUserMedia(mediaConstraints)
+    this.localVideo.nativeElement.srcObject = this.localStream
+  }
 }
