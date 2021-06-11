3eb67db3b1c602c99a4e50d7a6311c841ed36f8d
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-06-11 11:26:56 +0200

    💄 BreakpointObserver & reactivity

--- contacts/src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ contacts/src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+
+const SMALL_WIDTH_BREAKPOINT = 720
 
 @Component({
   selector: 'app-sidenav',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavComponent implements OnInit {
 
-  constructor() { }
+  public isScreenSmall!: boolean;
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
+    this.breakpointObserver
+    // .observe([ Breakpoints.XSmall ])
+    .observe([`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`])
+    .subscribe((state: BreakpointState) => {
+      this.isScreenSmall = state.matches
+    })
   }
 
 }
