f8250f6fe792b1b0a974be01e763881efaf13bb2
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-06-11 12:15:32 +0200

    💄 Content/toolbar/sidenav styling

--- contacts/src/app/contactmanager/components/toolbar/toolbar.component.ts
+++ contacts/src/app/contactmanager/components/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit {
+  @Output() toggleSidenav = new EventEmitter<void>();
 
   constructor() { }
 
