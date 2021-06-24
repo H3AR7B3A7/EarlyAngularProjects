521442708d70ce0ee6544598f1204f46ccec2e32
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-06-24 22:38:15 +0200

    ✨ Table and pagination

--- contacts/src/app/contactmanager/components/main-content/main-content.component.ts
+++ contacts/src/app/contactmanager/components/main-content/main-content.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Contact } from '../../models/Contact';
@@ -11,16 +13,20 @@ import { ContactService } from '../../services/contact.service';
   styleUrls: ['./main-content.component.scss']
 })
 export class MainContentComponent implements OnInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   contacts$?: Observable<Contact[]>
 
   currentListId: number = 0
 
+  displayedColumns: string[] = ['name', 'email', 'number']
+  contacts!: MatTableDataSource<Contact>
+
   constructor(
     private route: ActivatedRoute,
     private contactService: ContactService,
     private auth: AuthService,
-    private router: Router
+    private router: Router,
   ) {
     this.router.events.subscribe(() => this.loadContactsForList())
   }
@@ -29,6 +35,10 @@ export class MainContentComponent implements OnInit {
     this.auth.checkLoggedInStatus()
 
     this.contacts$ = this.contactService.contacts
+    this.contacts$.subscribe(data => {
+      this.contacts = new MatTableDataSource<Contact>(data)
+      this.contacts.paginator = this.paginator
+    })
   }
 
   private loadContactsForList() {
