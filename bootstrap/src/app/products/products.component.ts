16f757f1c9c11113a0ad23e501e94aba646ae1c6
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-06-27 12:53:42 +0200

    ✨ Data binding

--- bootstrap/src/app/products/products.component.ts
+++ bootstrap/src/app/products/products.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 export class ProductsComponent implements OnInit {
 
   title: string = 'Product List!'
+  imageHeight: number = 50
+  imageMargin: number = 2
+  showImage: boolean = false
+  filter: string = ''
+
   products: any[] = [
     {
       "productId": 1,
@@ -16,7 +21,7 @@ export class ProductsComponent implements OnInit {
       "description": "Leaf rake with 48-inch wooden handle.",
       "price": 19.95,
       "starRating": 3.2,
-      "imageUrl": "assets/images/leaf_rake.png"
+      "imageUrl": "https://www.sneeboer.com/wp-content/uploads/2021/04/8715093060450-1.jpg"
     },
     {
       "productId": 2,
@@ -26,13 +31,17 @@ export class ProductsComponent implements OnInit {
       "description": "15 gallon capacity rolling garden cart",
       "price": 32.99,
       "starRating": 4.2,
-      "imageUrl": "assets/images/garden_cart.png"
+      "imageUrl": "https://ak1.ostkcdn.com/images/products/is/images/direct/71e707ebc1b09d03f0971d2e8640f9b731967dfc/2-Tire-Wheelbarrow-Garden-Cart-Heavy-duty-Dolly-Utility-Cart.jpg"
     },
   ]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleImage(): void {
+    this.showImage = !this.showImage
   }
 
 }
