afe0b103794befdae43044cb9b8cc9ef54b9fd05
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-06-27 05:42:15 +0200

    ✨ Bootstrap Table of Products

--- bootstrap/src/app/app.component.ts
+++ bootstrap/src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div>
+    <h1>{{ title }}</h1>
+    <app-products></app-products>
+  </div>
+  `
 })
 export class AppComponent {
-  title = 'bootstrap';
+  title = 'Bootstrap';
 }
