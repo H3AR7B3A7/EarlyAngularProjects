6343a7f13bfc3ad559fb5cb4171c9a2daa5723fb
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-07-05 10:13:54 +0200

    ✨ Minor tweaks

--- volumeMeter/src/app/app.component.ts
+++ volumeMeter/src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   volume0 = 100
   volume1 = 70
   volume2 = 60
@@ -14,6 +14,13 @@ export class AppComponent {
   volume5 = 30
   volume6 = 20
   volume7 = 10
+  volume8 = 0
 
   path = './assets/images/e.png'
+
+  ngOnInit() {
+    setInterval(() => {
+      this.volume8 = Math.floor(Math.random() * 80)
+    }, 1000)
+  }
 }
