6a540640bd017f084fee623cd51efc7764f24112
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-07-11 17:54:08 +0200

    ✨ More sugar

--- modelForms/src/app/advanced-form/advanced-form.component.ts
+++ modelForms/src/app/advanced-form/advanced-form.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 
 interface Address {
   street: string,
-  number: string,
+  number: number,
   city: string,
   country: string
 }
@@ -23,15 +23,16 @@ interface Profile {
 })
 export class AdvancedFormComponent implements OnInit {
   title = 'Advanced Form'
+  mouseOverSave = false
 
-  private firstName!: FormControl
-  private lastName!: FormControl
-  private street!: FormControl
-  private number!: FormControl
-  private city!: FormControl
-  private country!: FormControl
-  private about?: FormControl
-  private img?: FormControl
+  firstName!: FormControl
+  lastName!: FormControl
+  street!: FormControl
+  number!: FormControl
+  city!: FormControl
+  country!: FormControl
+  about?: FormControl
+  img?: FormControl
 
   profileForm!: FormGroup
   address!: FormGroup
@@ -42,56 +43,60 @@ export class AdvancedFormComponent implements OnInit {
 
   ngOnInit(): void {
     // THE LONG WAY
-    // this.firstName = new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z].*')])
-    // this.lastName = new FormControl('', Validators.required)
-    // this.street = new FormControl('', Validators.required)
-    // this.number = new FormControl('', Validators.required)
-    // this.city = new FormControl('', Validators.required)
-    // this.country = new FormControl('', Validators.required)
-    // this.about = new FormControl('')
-    // this.img = new FormControl('')
-    // this.profileForm = new FormGroup({
-    //   firstName: this.firstName,
-    //   lastName: this.lastName,
-    //   address: new FormGroup({
-    //     street: this.street,
-    //     number: this.number,
-    //     city: this.city,
-    //     country: this.country
-    //   }),
-    //   about: this.about,
-    //   img: this.img
-    // })
-
-    this.profileForm = this._fb.group({
-      firstName: ['', [Validators.required, Validators.pattern('[a-zA-Z].*')]],
-      lastName: ['', Validators.required],
-      address: this._fb.group({
-        street: ['', Validators.required],
-        number: ['', Validators.required],
-        city: ['', Validators.required],
-        country: ['', Validators.required]
+    this.firstName = new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z].*')])
+    this.lastName = new FormControl('', Validators.required)
+    this.street = new FormControl('', Validators.required)
+    this.number = new FormControl('', Validators.required)
+    this.city = new FormControl('', Validators.required)
+    this.country = new FormControl('', Validators.required)
+    this.about = new FormControl('', Validators.maxLength(200))
+    this.img = new FormControl('', Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?'))
+    this.profileForm = new FormGroup({
+      firstName: this.firstName,
+      lastName: this.lastName,
+      address: new FormGroup({
+        street: this.street,
+        number: this.number,
+        city: this.city,
+        country: this.country
       }),
-      about: ['', Validators.required],
-      img: ['', Validators.required]
+      about: this.about,
+      img: this.img
     })
+
+    // this.profileForm = this._fb.group({
+    //   firstName: ['', [Validators.required, Validators.pattern('[a-zA-Z].*')]],
+    //   lastName: ['', Validators.required],
+    //   address: this._fb.group({
+    //     street: ['', Validators.required],
+    //     number: ['', Validators.required],
+    //     city: ['', Validators.required],
+    //     country: ['', Validators.required]
+    //   }),
+    //   about: ['', Validators.required],
+    //   img: ['', Validators.required]
+    // })
   }
 
   cancel() {
     console.log('Cancelled')
   }
 
-  saveProfile(value: any) {
-
+  saveProfile(formValues: any) {
     console.log('saved: ' + JSON.stringify(this.profileForm.value))
 
-  }
-
-  validateFirstName() {
-    return true // this.firstName.valid || this.firstName.untouched
-  }
-
-  validateLastName() {
-    return true // this.lastName.valid || this.lastName.untouched
+    let profile: Profile = {
+      firstName: formValues.firstName,
+      lastName: formValues.lastName,
+      address: {
+        street: formValues.address.street,
+        number: +formValues.address.number,
+        city: formValues.address.city,
+        country: formValues.address.country
+      },
+      about: formValues.about,
+      img: formValues.img
+    }
+    console.log('object: ' + JSON.stringify(profile))
   }
 }
