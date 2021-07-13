511cbf746243760df0836678f93171050c0a8887
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-07-13 19:15:26 +0200

    ✨ Toasty

--- toastExamples/src/app/app.component.ts
+++ toastExamples/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'toastExamples';
+
+  constructor(
+    private toastr: ToastrService
+  ) { }
+
+  ngOnInit() {
+    this.toastr.error('everything is broken', 'Major Error', {
+      timeOut: 3000,
+    });
+  }
+
+  successToast() {
+    this.toastr.success(undefined, 'Success') // Ignores preventDuplicates: true when undefined
+  }
+
+  infoToast() {
+    this.toastr.info('Some info...', 'Info', { timeOut: 3000 })
+  }
+
+  warningToast() {
+    this.toastr.warning('Some warning...', 'Warning', { closeButton: true })
+  }
+
+  clearToasts() {
+    this.toastr.clear()
+  }
 }
--- toastExamples/src/app/app.module.ts
+++ toastExamples/src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Toast, ToastrModule } from 'ngx-toastr';
 import { AppComponent } from './app.component';
+import { PinkToast } from './custom-toasts/pink.toast';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PinkToast
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      toastComponent: PinkToast, // Toast = default
+      positionClass: 'toast-bottom-center',
+      preventDuplicates: true,
+    })
   ],
+  entryComponents: [PinkToast],
   providers: [],
   bootstrap: [AppComponent]
 })
