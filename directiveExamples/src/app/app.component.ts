f9f24d86246e8c2c5fbc32bd0d27091286f34800
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-07-16 21:33:20 +0200

    ✨ Hold to delete

--- directiveExamples/src/app/app.component.ts
+++ directiveExamples/src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'directiveExamples';
+  title = 'Directive Examples';
+  progress = 0
+  message = ''
+
+  holdHandler($event: number) {
+    console.log('%c ' + $event, 'color: #6495ed')
+    this.progress = $event
+    if ($event == 1000) {
+      this.delete()
+    }
+  }
+
+  private delete() {
+    console.log('%c DELETED!!!', 'color: #ec6969; font-weight: bold')
+    this.message = "DELETED"
+    setTimeout(() => { this.message = '' }, 1000)
+  }
 }
