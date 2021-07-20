0a4860f860f883c71c022ec49ff1f99674160dc2
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-07-21 00:16:50 +0200

    ♻️ Cleaned up some imports

--- fundamentals/src/app/events/event-resolver.service.ts
+++ fundamentals/src/app/events/event-resolver.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core'
 import { ActivatedRouteSnapshot, Resolve } from '@angular/router'
 import { EventService } from './shared/event.service'
-import { map } from 'rxjs/operators'
 
 @Injectable()
 export class EventResolver implements Resolve<any> {
--- fundamentals/src/app/events/events-list-resolver.service.ts
+++ fundamentals/src/app/events/events-list-resolver.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core'
 import { Resolve } from '@angular/router'
 import { EventService } from './shared/event.service'
-import { map } from 'rxjs/operators'
 
 @Injectable()
 export class EventListResolver implements Resolve<any> {
-  constructor(private eventService:EventService) {
+  constructor(private eventService: EventService) {
 
   }
 
