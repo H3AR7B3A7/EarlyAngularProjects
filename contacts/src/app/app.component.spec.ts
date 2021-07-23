7448299d33e7c9171e30d44c959570d8bf4641bb
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-07-24 00:47:44 +0200

    🔥 Removed a test

--- contacts/src/app/app.component.spec.ts
+++ contacts/src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('contacts');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('contacts app is running!');
-  });
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement;
+  //   expect(compiled.querySelector('.content span').textContent).toContain('contacts app is running!');
+  // });
 });
