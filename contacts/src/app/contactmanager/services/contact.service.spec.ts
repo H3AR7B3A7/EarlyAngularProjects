cb44d30647a039370dca85f5df8cce9a4d9d5f6a
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-07-25 01:57:43 +0200

    ✨ More tests

--- contacts/src/app/contactmanager/services/contact.service.spec.ts
+++ contacts/src/app/contactmanager/services/contact.service.spec.ts
@@ -6,7 +6,7 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 import { HttpClient } from '@angular/common/http'
 import { of } from 'rxjs'
 
-describe('ContactService', () => {
+describe('ContactService: ContactLists', () => {
   let injector: TestBed
   let contactService: ContactService
   let contactServiceMock: ContactService
@@ -15,8 +15,14 @@ describe('ContactService', () => {
 
   beforeEach(() => {
     mockHttp = {
-      get: (x: string) => {
-        return of(CONTACT_LISTS.filter(list => list.userId === +x.substring(x.length - 1, x.length)))
+      get: (s: string) => {
+        return of(CONTACT_LISTS.filter(list => list.userId === +s.substring(s.length - 1, s.length)))
+      },
+      post: (s: string, l: ContactList) => {
+        return of(l)
+      },
+      delete: (s: string) => {
+        return of(CONTACT_LISTS.filter(list => list.id !== +s.substring(s.length - 1, s.length)))
       }
     }
 
@@ -35,7 +41,7 @@ describe('ContactService', () => {
   })
 
   describe('loadContactLists', () => {
-    it('Should make GET request to right URI>', () => {
+    it('Should make a GET request to the right URI.', () => {
       const userId = 1
 
       contactService.loadContactLists(userId)
@@ -51,20 +57,54 @@ describe('ContactService', () => {
 
       contactServiceMock.contactLists.subscribe(lists => {
         expect(lists.length).toBe(2)
-        expect(lists).toEqual(CONTACT_LISTS.filter(list => list.userId === 1))
+        expect(lists).toEqual(CONTACT_LISTS.filter(list => list.userId === userId))
         expect(contactServiceMock.dataStore.contactLists.length).toBe(2)
       })
     })
   })
 
   describe('addContactList', () => {
-    it('Should add a ContactList', () => {
+    it('Should make a POST request to the right URI.', () => {
       const newList: ContactList = { id: 4, userId: 1, name: 'list4' }
 
-      // contactService.addContactList(newList)
-      // contactService.contactLists.subscribe(() => {
-      //   expect(contactService.dataStore.contactLists.length).toBe(1)
-      // })
+      contactService.addContactList(newList)
+
+      const req = httpMock.expectOne(contactService.API + 'lists/')
+      expect(req.request.method).toBe('POST')
+    })
+
+    it('Should add a ContactList to data store', () => {
+      const newList: ContactList = { id: 4, userId: 1, name: 'list4' }
+
+      contactServiceMock.addContactList(newList)
+
+      contactServiceMock.contactLists.subscribe(() => {
+        expect(contactServiceMock.dataStore.contactLists.length).toBe(1)
+        expect(contactServiceMock.dataStore.contactLists).toEqual([newList])
+      })
+    })
+  })
+
+  describe('deleteContactList', () => {
+    it('Should make a DELETE request to the right URI.', () => {
+      const id = 1
+
+      contactService.deleteContactList(id)
+
+      const req = httpMock.expectOne(contactService.API + 'delete/lists/' + id)
+      expect(req.request.method).toBe('DELETE')
+    })
+
+    it('Should remove a ContactList from the data store', () => {
+      const id = 1
+
+      contactServiceMock.dataStore.contactLists = [...CONTACT_LISTS]
+      contactServiceMock.deleteContactList(id)
+
+      contactServiceMock.contactLists.subscribe(() => {
+        expect(contactServiceMock.dataStore.contactLists.length).toBe(2)
+        expect(contactServiceMock.dataStore.contactLists).toEqual(CONTACT_LISTS.filter(l => l.id !== id))
+      })
     })
   })
 })
