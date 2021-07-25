d114a4064b94a78abc5474f174bbcda315f3689c
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-07-25 21:12:07 +0200

    ✨ Simple isolated guard tests

--- contacts/src/app/contactmanager/services/auth.guard.spec.ts
+++ contacts/src/app/contactmanager/services/auth.guard.spec.ts
@@ -1,16 +1,65 @@
 import { TestBed } from '@angular/core/testing'
-
+import { ActivatedRouteSnapshot, Router } from '@angular/router'
 import { AuthGuard } from './auth.guard'
+import { AuthService } from './auth.service'
+import { RouterStateSnapshot } from '@angular/router'
+import { RouterTestingModule } from '@angular/router/testing'
 
-describe('AuthGuardGuard', () => {
+describe('AuthGuard', () => {
   let guard: AuthGuard
 
   beforeEach(() => {
-    TestBed.configureTestingModule({})
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule]
+    })
     guard = TestBed.inject(AuthGuard)
   })
 
-  it('should be created', () => {
+  it('Should be created.', () => {
     expect(guard).toBeTruthy()
   })
 })
+
+describe('AuthGuard (isolated)', () => {
+  beforeEach(() => {
+    routerSpy = jasmine.createSpyObj<Router>('Router', ['navigate'])
+    serviceStub = {}
+    guard = new AuthGuard(serviceStub as AuthService, routerSpy)
+  })
+
+  const dummyRoute = {} as ActivatedRouteSnapshot
+  const protectedUrls = ['/', '/contacts', '/contacts/1']
+  const allowedUrls = ['/contacts/login']
+  let guard: AuthGuard
+  let routerSpy: jasmine.SpyObj<Router>
+  let serviceStub: Partial<AuthService>
+
+  describe('When the user is logged in:', () => {
+    beforeEach(() => {
+      serviceStub.isLoggedIn = true
+    })
+
+    it('Access is granted.', () => {
+      const isAccessGranted = guard.canActivate()
+      expect(isAccessGranted).toBeTrue()
+    })
+  })
+
+  describe('When the user is logged out:', () => {
+    beforeEach(() => {
+      serviceStub.isLoggedIn = false
+    })
+
+    it('Access is denied.', () => {
+      const isAccessGranted = guard.canActivate()
+      expect(isAccessGranted).toBeFalsy()
+    })
+  })
+})
+
+
+function fakeRouterState(url: string): RouterStateSnapshot {
+  return {
+    url,
+  } as RouterStateSnapshot
+}
