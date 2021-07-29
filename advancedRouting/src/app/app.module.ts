c8ad094945e93ecbcc7771081a66b01a4b6e04d9
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-07-29 23:25:37 +0200

    ✨ Basic routing

--- advancedRouting/src/app/app.module.ts
+++ advancedRouting/src/app/app.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { HttpClientModule } from '@angular/common/http'
 import { RouterModule } from '@angular/router'
-
-// Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api'
 import { ProductData } from './products/product-data'
-
 import { AppComponent } from './app.component'
 import { WelcomeComponent } from './home/welcome.component'
 import { PageNotFoundComponent } from './page-not-found.component'
-
-/* Feature Modules */
 import { ProductModule } from './products/product.module'
 import { UserModule } from './user/user.module'
 import { MessageModule } from './messages/message.module'
