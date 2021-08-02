68ff84f3978ad7f41dfcebb1509a7e1bed597853
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-08-02 23:02:56 +0200

    ✨ Component-less route

--- advancedRouting/src/app/products/product.module.ts
+++ advancedRouting/src/app/products/product.module.ts
@@ -13,29 +13,37 @@ import { ProductEditInfoComponent } from './product-edit/product-edit-info.compo
   imports: [
     SharedModule,
     RouterModule.forChild([
-      { path: 'products', component: ProductListComponent },
       {
-        path: 'products/:id',
-        component: ProductDetailComponent,
-        resolve: { resolvedData: ProductResolver }
-      },
-      {
-        path: 'products/:id/edit',
-        component: ProductEditComponent,
-        resolve: { resolvedData: ProductResolver },
+        path: 'products',
         children: [
           {
             path: '',
-            redirectTo: 'info',
-            pathMatch: 'full'
+            component: ProductListComponent
           },
           {
-            path: 'info',
-            component: ProductEditInfoComponent
+            path: ':id',
+            component: ProductDetailComponent,
+            resolve: { resolvedData: ProductResolver }
           },
           {
-            path: 'tags',
-            component: ProductEditTagsComponent
+            path: ':id/edit',
+            component: ProductEditComponent,
+            resolve: { resolvedData: ProductResolver },
+            children: [
+              {
+                path: '',
+                redirectTo: 'info',
+                pathMatch: 'full'
+              },
+              {
+                path: 'info',
+                component: ProductEditInfoComponent
+              },
+              {
+                path: 'tags',
+                component: ProductEditTagsComponent
+              }
+            ]
           }
         ]
       }
