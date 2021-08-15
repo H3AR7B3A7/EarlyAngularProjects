fc0a34df61029b9a78c81e5dbd5f806848717604
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-08-15 22:35:20 +0200

    ✨ Nested FormGroups

--- modelForms2/src/app/customers/customer.component.ts
+++ modelForms2/src/app/customers/customer.component.ts
@@ -41,7 +41,10 @@ export class CustomerComponent implements OnInit {
     this.customerForm = this.fb.group({
       firstName: ['', [Validators.required, Validators.minLength(3)]],
       lastName: ['', [Validators.required, Validators.maxLength(50)]],
-      email: ['', [Validators.required, Validators.email]],
+      emailGroup: this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        confirmEmail: ['', Validators.required],
+      }),
       phone: '',
       notification: 'email',
       rating: [null, ratingRange(1, 5)],
