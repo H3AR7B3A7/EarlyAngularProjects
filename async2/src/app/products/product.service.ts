f38e636a466a564e4e0a3a0fe05b3b17284f3893
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-08-25 21:45:08 +0200

    ✨ Mapping object array from http request

--- async2/src/app/products/product.service.ts
+++ async2/src/app/products/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Product } from './product';
 import { Supplier } from '../suppliers/supplier';
@@ -17,12 +17,22 @@ export class ProductService {
 
   products$ = this.http.get<Product[]>(this.productsUrl)
     .pipe(
+      // map(item => item.price * 1.5),
+      map(products =>
+        products.map(product => ({
+          ...product,
+          price: product.price * 1.5,
+          searchKey: [product.productName]
+        }) as Product)
+      ),
       tap(data => console.log('Products: ', JSON.stringify(data))),
       catchError(this.handleError)
     );
 
-  constructor(private http: HttpClient,
-              private supplierService: SupplierService) { }
+  constructor(
+    private http: HttpClient,
+    private supplierService: SupplierService
+  ) { }
 
   private fakeProduct() {
     return {
