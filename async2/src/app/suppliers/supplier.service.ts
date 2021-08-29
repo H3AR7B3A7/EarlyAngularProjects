ebab8d180f0ff51d6a6bf2ddafa12bf363f8ee6e
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-08-29 23:35:02 +0200

    ✨ Higher-order mapping operators

--- async2/src/app/suppliers/supplier.service.ts
+++ async2/src/app/suppliers/supplier.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 
-import { throwError } from 'rxjs'
+import { of, throwError } from 'rxjs'
+import { Supplier } from './supplier'
+import { concatMap, map, mergeMap, switchMap, tap } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,46 @@ import { throwError } from 'rxjs'
 export class SupplierService {
   suppliersUrl = 'api/suppliers'
 
-  constructor(private http: HttpClient) { }
+  suppliersWithMap$ = of(1, 5, 8) // Bad practice
+    .pipe(
+      map(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+    )
+
+  suppliersWithConcatMap$ = of(1, 5, 8) // In sequence
+    .pipe(
+      tap(id => console.log('concatMap source Observable', id)),
+      concatMap(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+    )
+
+  suppliersWithMergeMap$ = of(1, 5, 8) // Parallel
+    .pipe(
+      tap(id => console.log('concatMap source Observable', id)),
+      mergeMap(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+    )
+
+  suppliersWithSwitchMap$ = of(1, 5, 8) // Last only
+    .pipe(
+      tap(id => console.log('concatMap source Observable', id)),
+      switchMap(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+    )
+
+  constructor(
+    private http: HttpClient
+  ) {
+    // this.suppliersWithMap$
+    //   .subscribe(o => o.subscribe(
+    //     item => console.log('map result', item)
+    //   ))
+    // this.suppliersWithConcatMap$.subscribe(
+    //   item => console.log('concatMap result', item)
+    // )
+    // this.suppliersWithMergeMap$.subscribe(
+    //   item => console.log('concatMap result', item)
+    // )
+    // this.suppliersWithSwitchMap$.subscribe(
+    //   item => console.log('concatMap result', item)
+    // )
+  }
 
   private handleError(err: any) {
     // in a real world app, we may send the server to some remote logging infrastructure
