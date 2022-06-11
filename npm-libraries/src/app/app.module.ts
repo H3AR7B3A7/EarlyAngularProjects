1ac2889d230bcef4ad4a67950d48b64b216efc9f
Steven D'Hondt <48164405+H3AR7B3A7@users.noreply.github.com>
2022-06-11 12:50:01 +0000

    ♻️ Refactor

--- npm-libraries/src/app/app.module.ts
+++ npm-libraries/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {
-  SpinAnythingModule,
-} from 'projects/spin-anything/src/public-api';
+import { SpinAnythingModule } from 'spin-anything';
 
 import { AppComponent } from './app.component';
 
