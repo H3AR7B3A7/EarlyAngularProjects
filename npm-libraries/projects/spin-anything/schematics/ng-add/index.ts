275db8312af20f6d5f0e9e70df9bec1af63d3f5d
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2022-06-12 00:08:12 +0200

    🐛 Fix for schematics

--- npm-libraries/projects/spin-anything/schematics/ng-add/index.ts
+++ npm-libraries/projects/spin-anything/schematics/ng-add/index.ts
@@ -2,7 +2,7 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 
 // Just return the tree
-export function ngAdd(): Rule {
+export default function (): Rule {
     return (tree: Tree, context: SchematicContext) => {
         context.addTask(new NodePackageInstallTask());
         return tree;
